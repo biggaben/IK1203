898adb00918d4263a85c4c9743d30f4b87afc8b8
BigGaben <david.holmertz93@gmail.com>
2024-01-21 08:41:19 +0100

    Fuctional Client and Server

--- Tasks/Task 1/Socket/TCPClient.java
+++ Tasks/Task 1/Socket/TCPClient.java
@@ -1,11 +1,45 @@
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
-import java.io.IOException;
 public class TCPClient {
-    
+    // Method for handling three arguments (hostname, port and userInput)
+    public static String askServer(String hostname, int port, String userInputString) throws IOException {
+        try (
+            Socket socket = new Socket(hostname, port); // Create a new Socket and connect it to the server
+            OutputStream outputStream = socket.getOutputStream(); // Get the output stream from the socket
+            InputStream inputStream = socket.getInputStream(); // Get the input stream from the socket
+        ){
+            int bufferSize = 1024;
+            byte[] bufferServer = new byte[bufferSize];
+
+            //# If the userInputString is longer than the buffer size, then only send the first bufferSize characters
+            byte[] userInputArray;
+            userInputArray = userInputString.getBytes(StandardCharsets.UTF_8);
+            outputStream.write(userInputArray);
+            outputStream.flush();
+
+            // Create a new ByteArrayOutputStream to store the response from the server
+            ByteArrayOutputStream responseStream = new ByteArrayOutputStream();
+
+            try {
+                while (inputStream.read(bufferServer) != -1) {
+                    responseStream.write(bufferServer, 0, 1024); 
+                }
+            } catch (IOException e) {
+                System.err.println(e);
+            }
+
+
+            byte[] outputData = responseStream.toByteArray();
+            return new String(outputData, StandardCharsets.UTF_8);
+        }
+    }
+
+
+
     // Method for handling two arguments (hostname and port)
     public static String askServer(String hostname, int port) throws IOException {
         try (
@@ -13,38 +47,13 @@ public class TCPClient {
             OutputStream outputStream = socket.getOutputStream(); // Get the output stream from the socket
             InputStream inputStream = socket.getInputStream(); // Get the input stream from the socket
         ){
+            int bufferSize = 1024;
+            byte[] bufferServer = new byte[bufferSize]; // Create a buffer to store the response from the server
             ByteArrayOutputStream responseStream = new ByteArrayOutputStream(); // Create a new ByteArrayOutputStream to store the response from the server
-            byte[] buffer = new byte[1024]; // Create a buffer to store the response from the server
-            int bytesRead;  // Create a variable to store the number of bytes read from the server
-        
-            try {
-                while ((bytesRead = inputStream.read(buffer)) != -1) {
-                    responseStream.write(buffer, 0, bytesRead);   
-                }
-            } catch (IOException e) {
-                System.err.println(e);
-            }
 
-            byte[] outputData = responseStream.toByteArray();
-            return new String(outputData, StandardCharsets.UTF_8);
-        }
-    }
-
-    public static String askServer(String hostname, int port, String stringToServer) throws IOException {
-        try (
-            Socket socket = new Socket(hostname, port); // Create a new Socket and connect it to the server
-            OutputStream outputStream = socket.getOutputStream(); // Get the output stream from the socket
-            InputStream inputStream = socket.getInputStream(); // Get the input stream from the socket
-        ){
-            byte[] bytesToServer = stringToServer.getBytes(StandardCharsets.UTF_8);
-            outputStream.write(bytesToServer); // Send bytes to server
-            ByteArrayOutputStream responseStream = new ByteArrayOutputStream(); // Create a new ByteArrayOutputStream to store the response from the server
-            byte[] buffer = new byte[1024]; // Create a buffer to store the response from the server
-            int bytesRead;  // Create a variable to store the number of bytes read from the server
-        
             try {
-                while ((bytesRead = inputStream.read(buffer)) != -1) {
-                    responseStream.write(buffer, 0, bytesRead);   
+                while (inputStream.read(bufferServer) != -1) {
+                    responseStream.write(bufferServer, 0, 1024);   
                 }
             } catch (IOException e) {
                 System.err.println(e);
